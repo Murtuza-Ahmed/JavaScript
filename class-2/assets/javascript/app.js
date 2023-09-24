1143ee1e92b71091e405548c3a7f6c534c5055ab
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-09-24 14:53:48 +0500

    commint out

--- class-2/assets/javascript/app.js
+++ class-2/assets/javascript/app.js
@@ -2,34 +2,34 @@
 
 
 
-// prompt("Enter your massege");
-// console.log(Math.random() * 50);
-// console.log(Math.floor(Math.random() * 50));
+prompt("Enter your massege");
+console.log(Math.random() * 50);
+console.log(Math.floor(Math.random() * 50));
 
 // ====================================Chapter-2========================================
 
-// let name = "Murtuza";
-// console.log(name);
+let name = "Murtuza";
+console.log(name);
 
-// let number = "30";
-// console.log(number);
+let number = "30";
+console.log(number);
 
-// const pi = 3.142;
-// pi = 4;
-// console.log(pi);
+const pi = 3.142;
+pi = 4;
+console.log(pi);
 
-// let funActivity = "let's learn javascript";
-// console.log(funActivity);
+let funActivity = "let's learn javascript";
+console.log(funActivity);
 
-// let question = 'do you want to learn javascript! "yes"';
-// console.log(question)
+let question = 'do you want to learn javascript! "yes"';
+console.log(question)
 
-// let language = "javascript";
-// let message = `let's learn ${language}`;
-// console.log(message);
+let language = "javascript";
+let message = `let's learn ${language}`;
+console.log(message);
 
-// let str3 = "new \nline";
-// console.log(str3);
+let str3 = "new \nline";
+console.log(str3);
 
-// let firstName = "Murtuza Ahmed";
-// alert(firstName);
+let firstName = "Murtuza Ahmed";
+alert(firstName);
