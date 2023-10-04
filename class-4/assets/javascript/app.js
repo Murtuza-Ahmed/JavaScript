4cde207fe27ce202105b0557eca51242dc4f0f2e
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-10-04 22:17:56 +0500

    pythagorestieorem

---
+++ class-4/assets/javascript/app.js
@@ -0,0 +1,16 @@
+// let nr1 = 12;
+// let nr2 = 14;
+// let result = nr1 + nr2;
+// console.log(result);
+
+// let nr1 = 12;
+// let nr2 = 14;
+// let result = nr1 / nr2;
+// console.log(result);
+
+let a = +prompt("enter your number");
+let b = +prompt("enter your number");
+let c = a**2;
+let d = b**2;
+let e = (c + d)**0.5;
+console.log(e);
