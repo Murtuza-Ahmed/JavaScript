d92ad3afc99c104ccfcd43485a8937c44bb1ab86
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-10-05 09:18:53 +0500

    class-4 end

--- class-4/assets/javascript/app.js
+++ class-4/assets/javascript/app.js
@@ -8,9 +8,21 @@
 // let result = nr1 / nr2;
 // console.log(result);
 
-let a = +prompt("enter your number");
-let b = +prompt("enter your number");
-let c = a**2;
-let d = b**2;
-let e = (c + d)**0.5;
-console.log(e);
+// let nr1 = 8;
+// let nr2 = 2;
+// let result = nr1 + nr2;
+// console.log(`${nr1} + ${nr2} = ${result}`);
+
+// let a = +prompt("enter your number");
+// let b = +prompt("enter your number");
+// let c = a**2;
+// let d = b**2;
+// let e = (c + d)**0.5;
+// console.log(e);
+
+let a = parseInt(prompt("Enter the Value"));
+let b = parseInt(prompt("Enter the Value"));
+let c = a**2 + b**2;
+let d = c**0.5;
+console.log(d);
+console.log(`${c} + ${d}`); 
