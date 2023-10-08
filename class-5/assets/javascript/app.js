4fa89e80b0cb03de252dd7878c47d8e47f0b526e
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-10-08 20:09:29 +0500

    comparison operators

---
+++ class-5/assets/javascript/app.js
@@ -0,0 +1,37 @@
+// let w = +prompt("Enter Value");
+// let x = +prompt("Enter Value");
+// let y = x**2;
+// let z = y**2;
+// let c;
+
+// VALUE ASSIGMINT
+
+// let x = 2;
+// x += 2;
+// console.log(x);
+
+// let z = 2;
+// z -= 2;
+// console.log(z);
+
+// let a = 2;
+// a *= 2;
+// console.log(a);
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// a += b;
+// a /= c;
+// c = c % b;
+// console.log(c);
+
+// COMPARISON OPERATORS
+
+// let x = 5;
+// let y = "5";
+// console.log(x == y);
+
+// let x = 5;
+// let y = "5";
+// console.log(x === y);
