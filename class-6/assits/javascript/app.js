4fa0149ae73ec5283f662f5bea8f05dfa9d7d061
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-10-17 00:33:04 -0700

    symbol paractice

--- class-6/assits/javascript/app.js
+++ class-6/assits/javascript/app.js
@@ -1,3 +1,23 @@
-let miles = +prompt("Enter the Value");
-let update = Math.floor(miles * 1.60935);
-console.log(update);
+// let miles = +prompt("Enter the Value");
+// let update = Math.floor(miles * 1.60935);
+// console.log(update);
+
+// let weight = +prompt("Enter the Weight", 0);
+// let height = +prompt("Enter the Height", 0);
+// // let updateHeight = height * 2.54;
+// let update = weight / height ** 2;
+// console.log(update);
+
+// let weight = +prompt("Enter your Weight in KG");
+// let height = +prompt("Enter your Height in Feet");
+// let heightUpdate = height * 0.304;
+// let BMI = weight / heightUpdate ** 2;
+// console.log(BMI);
+
+// let sym = "javascript is fun";
+// let sym1 = "javascript is fun";
+// console.log(sym === sym1);
+
+let sym = Symbol("javascript is fun");
+let sym1 = Symbol("javascript is fun");
+console.log(sym === sym1);
