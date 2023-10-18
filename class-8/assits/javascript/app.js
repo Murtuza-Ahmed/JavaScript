e5498e04eec8c68dcb45f959c11b70981c0b59d3
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-10-18 09:59:02 -0700

    bad paractice

---
+++ class-8/assits/javascript/app.js
@@ -0,0 +1,21 @@
+// let a = true;
+// console.log(a);
+// if (a) {
+//     console.log("Hi")
+// }
+// if (!a) {
+//     console.log("By")
+// }
+
+// let userNumber = +prompt("Enter your Age");
+// let message;
+// if (userNumber > 21) {
+//     message = "WellCome";
+// } else if (userNumber > 19) {
+//     message = "Not WellCome";
+// } else if (userNumber > 30) {
+//     message = "Luser";
+// } else {
+//     message = "OK";
+// }
+// console.log(userNumber);
