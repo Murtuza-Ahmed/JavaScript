839132eb3a077856f2aab723984a89db4724430d
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-10-24 00:33:41 -0700

    stwitch statmnet paractice

--- class-9/assits/javascript/app.js
+++ class-9/assits/javascript/app.js
@@ -2,3 +2,19 @@
 // console.log(randomNumber);
 // let answer = "Some thing what Wrong";
 // let output = prompt("Ask me any thing");
+
+// let activity = "Dinner";
+// if (activity === "Get Up") {
+//     console.log("It's 8:00 AM");
+// } else if (activity === "Brak fast") {
+//     console.log("9:00 AM");
+// } else if (activity === "My Work end") {
+//     console.log("it's 12:00 Pm");
+// } else if (activity === "Dinner") {
+//     console.log("2:30PM");
+// } else if (activity === "Day Sleep") {
+//     console.log("3:00 pm time")
+// } else {
+//     console.log("No But Try");
+// }
+// console.log(activity);
