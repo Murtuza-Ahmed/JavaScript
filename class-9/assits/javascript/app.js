80f484b19e333ce7a687528f5e299ca9bdcca998
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-10-24 00:45:34 -0700

    paractice

--- class-9/assits/javascript/app.js
+++ class-9/assits/javascript/app.js
@@ -18,3 +18,25 @@
 //     console.log("No But Try");
 // }
 // console.log(activity);
+
+// let activity = "sleep";
+// switch (activity) {
+//     case "get up":
+//         console.log("it's 8:00 am");
+//         break;
+//     case "break fast":
+//         console.log("it's 9:00 am");
+//         break;
+//     case "my paractice work":
+//         console.log("it's 12:00 pm");
+//         break;
+//     case "dinner":
+//         console.log("it's 2:30pm");
+//         break;
+//     case "sleep day":
+//         console.log("it's 3:00 pm");
+//         break;
+//     default:
+//         console.log("no but try");
+// }
+// console.log(activity);
