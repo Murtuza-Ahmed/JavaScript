5f5f22b5bdbf674b93f98344d1919ca7438ec1a3
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-10-25 08:46:54 -0700

    paractice

---
+++ class-10/assits/javascript/app.js
@@ -0,0 +1,15 @@
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let a = 10;
+// while (a >= 0) {
+//     console.log(a);
+//     a--;
+// }
+
+let i = 10;
+let randomNumber = Math.floor(Math.random() * i) + 1;
+console.log(randomNumber);
