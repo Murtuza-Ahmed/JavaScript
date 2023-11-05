477ee35e0ede9d07b7989c3622622f787e5a3b1e
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-11-05 01:00:38 -0800

    paractice

---
+++ class-13/assits/javascript/app.js
@@ -0,0 +1,12 @@
+function addNumbers(a, b) {
+  const result = a + b;
+  return result;
+}
+var variable1 = 10;
+var variable2 = 5;
+var result1 = addNumbers(variable1, variable2);
+console.log("Result of the first call to addNumbers:", result1);
+var variable3 = 7;
+var variable4 = 3;
+var result2 = addNumbers(variable3, variable4);
+console.log("Result of the second call to addNumbers:", result2);
