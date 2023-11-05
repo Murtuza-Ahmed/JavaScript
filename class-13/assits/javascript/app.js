bb3dd258f086227ecb72dd3cc20ecef4c8c492dd
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-11-05 02:02:29 -0800

    paractice

--- class-13/assits/javascript/app.js
+++ class-13/assits/javascript/app.js
@@ -1,16 +1,19 @@
-var descriptiveWords = ["beautiful", "excellent", "super"];
-function randomNameDescription() {
-  let name = prompt("Please enter a name:");
-  return name;
-}
-function getRandomDescription() {
-  let randomIndex = Math.floor(Math.random() * descriptiveWords.length);
-  return descriptiveWords[randomIndex];
-}
-function describeRandomName() {
-  let name = randomNameDescription();
-  let description = getRandomDescription();
-  console.log("The name you entered: " + name);
-  console.log("Random description: " + description);
-}
-describeRandomName();
+// var descriptiveWords = ["beautiful", "excellent", "super"];
+// function randomNameDescription() {
+//   let name = prompt("Please enter a name:");
+//   return name;
+// }
+// function getRandomDescription() {
+//   let randomIndex = Math.floor(Math.random() * descriptiveWords.length);
+//   return descriptiveWords[randomIndex];
+// }
+// function describeRandomName() {
+//   let name = randomNameDescription();
+//   let description = getRandomDescription();
+//   console.log("The name you entered: " + name);
+//   console.log("Random description: " + description);
+// }
+// describeRandomName();
+
+let sayHi = () => console.log("hi");
+sayHi();
