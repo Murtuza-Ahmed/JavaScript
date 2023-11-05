f5e29f1e140946c2bed937aa06ca204940ce07fc
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-11-05 01:21:33 -0700

    class compelete

--- class-11/assits/javascript/app.js
+++ class-11/assits/javascript/app.js
@@ -1,11 +1,21 @@
-const myWork = [];
-for (let i = 1; i <= 10; i++) {
-  const lessonName = `Lesson ${i}`;
-  const isRunningThisYear = i % 2 === 0 ? true : false;
-  const tempLesson = {
-    name: lessonName,
-    status: isRunningThisYear,
-  };
-  myWork.push(tempLesson);
+// const myWork = [];
+// for (let i = 1; i <= 10; i++) {
+//   const lessonName = `Lesson ${i}`;
+//   const isRunningThisYear = i % 2 === 0 ? true : false;
+//   const tempLesson = {
+//     name: lessonName,
+//     status: isRunningThisYear,
+//   };
+//   myWork.push(tempLesson);
+// }
+// console.log(myWork);
+
+let arrOfArrays = [];
+for (let i = 0; i < 2; i++) {
+  arrOfArrays.push([]);
+  for (let j = 0; j < 3; j++) {
+    arrOfArrays[i].push(j);
+  }
 }
-console.log(myWork);
+// console.log(arrOfArrays);
+console.table(arrOfArrays);
