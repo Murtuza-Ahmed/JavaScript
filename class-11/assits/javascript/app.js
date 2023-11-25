59dbbcfa8e391a0d2571e476f0e1bffc24544820
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-11-25 20:23:58 +0500

    comint out

--- class-11/assits/javascript/app.js
+++ class-11/assits/javascript/app.js
@@ -10,12 +10,12 @@
 // }
 // console.log(myWork);
 
-let arrOfArrays = [];
-for (let i = 0; i < 2; i++) {
-  arrOfArrays.push([]);
-  for (let j = 0; j < 3; j++) {
-    arrOfArrays[i].push(j);
-  }
-}
-// console.log(arrOfArrays);
-console.table(arrOfArrays);
+// let arrOfArrays = [];
+// for (let i = 0; i < 2; i++) {
+//   arrOfArrays.push([]);
+//   for (let j = 0; j < 3; j++) {
+//     arrOfArrays[i].push(j);
+//   }
+// }
+// // console.log(arrOfArrays);
+// console.table(arrOfArrays);
