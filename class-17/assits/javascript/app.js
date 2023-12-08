cc4a3b9545283ba808addc5d554488edadc6f0de
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-12-08 09:38:28 +0500

    paractice

--- class-17/assits/javascript/app.js
+++ class-17/assits/javascript/app.js
@@ -1,24 +1,16 @@
-// function checkEmail() {
-//   const emailInput = document.getElementById("emailInput");
-//   const email = emailInput.value.trim();
-//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//   if (emailRegex.test(email)) {
-//     displayResult("Valid Email", "green");
-//   } else {
-//     displayResult("Invalid Email", "red");
-//   }
-// }
-
-// function displayResult(message, color) {
-//   const outputDiv = document.querySelector(".output");
-//   outputDiv.textContent = message;
-//   outputDiv.style.color = color;
-// }
-
 function checkEmail() {
-  let emailInput = document.getElementById("emailInput");
-  let email = emailInput.value;
-  const emailRegex = /[a-zA-Z0-9.-_]+@+[a-zA-Z0-9.-_]+.+[a-zA-Z0-9.-_]/;
-  if (emailRegex) {
+  const emailInput = document.getElementById("emailInput");
+  const email = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email)) {
+    displayResult("Valid Email", "green");
+  } else {
+    displayResult("Invalid Email", "red");
   }
+}
+
+function displayResult(message, color) {
+  const outputDiv = document.querySelector(".output");
+  outputDiv.textContent = message;
+  outputDiv.style.color = color;
 }
