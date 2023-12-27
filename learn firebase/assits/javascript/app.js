d32ade3ed04024ead675fa98400ff98eb1ac2a16
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-12-27 12:11:43 +0500

    paractice

---
+++ learn firebase/assits/javascript/app.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDlk5pFHaqftV0eVGelT9TTxKengKsAtA8",
+  authDomain: "learn-firebase-7e342.firebaseapp.com",
+  projectId: "learn-firebase-7e342",
+  storageBucket: "learn-firebase-7e342.appspot.com",
+  messagingSenderId: "162995412127",
+  appId: "1:162995412127:web:fe6c4a847df7ff099b96aa",
+  measurementId: "G-1W2JK7HBXT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
