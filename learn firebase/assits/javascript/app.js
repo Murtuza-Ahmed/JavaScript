80425f994ca231b1631a1b93e4d31a5ca5f94f64
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-12-27 13:25:39 +0500

    paractice

--- learn firebase/assits/javascript/app.js
+++ learn firebase/assits/javascript/app.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDlk5pFHaqftV0eVGelT9TTxKengKsAtA8",
   authDomain: "learn-firebase-7e342.firebaseapp.com",
   projectId: "learn-firebase-7e342",
@@ -6,7 +6,15 @@ const firebaseConfig = {
   messagingSenderId: "162995412127",
   appId: "1:162995412127:web:fe6c4a847df7ff099b96aa",
   measurementId: "G-1W2JK7HBXT",
-};
+});
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+const auth = firebaseApp.auth();
+function signup() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  console.log(`Email: ${email}`);
+  console.log(`Password: ${password}`);
+  
+}
