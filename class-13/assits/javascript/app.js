cf0cad1d3f7b620ccab190d107cbae423ccb1262
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-12-30 12:19:28 +0500

    paractice loop

--- class-13/assits/javascript/app.js
+++ class-13/assits/javascript/app.js
@@ -24,30 +24,63 @@
 //   i++;
 // }
 
-let userFriends = prompt("Enter The Name");
-let friendHere = userFriends.toLowerCase();
-console.log(friendHere);
-let nameArray = ["Hamza", "Umer", "Zubair", "Murtuza"];
-let notFound = true;
-while (notFound && nameArray.length > 0) {
-  if (nameArray[0] === friendHere) {
-    console.log("Found her!");
-    notFound = false;
-  } else {
-    nameArray.shift();
+// let userFriends = prompt("Enter The Name");
+// let friendHere = userFriends.toLowerCase();
+// console.log(friendHere);
+// let nameArray = ["Hamza", "Umer", "Zubair", "Murtuza"];
+// let notFound = true;
+// while (notFound && nameArray.length > 0) {
+//   if (nameArray[0] === friendHere) {
+//     console.log("Found her!");
+//     notFound = false;
+//   } else {
+//     nameArray.shift();
+//   }
+// }
+
+// let maxNumber = 10;
+// let genrateNumber = Math.floor(Math.random() * maxNumber + 1);
+// let isCorrect = false;
+// while (!isCorrect) {
+//   let userNumber = +prompt(`Enter The Number! ${maxNumber}`);
+//   if (userNumber === genrateNumber) {
+//     isCorrect = true;
+//     console.log("Congrulation! Your Guess is Correct!");
+//   } else if (userNumber > genrateNumber) {
+//     console.log("To High! Try Again");
+//   } else {
+//   }
+// }
+
+// let tableNumber = +prompt("Enter Table Number");
+// let message;
+// for (let i = 1; i <= 10; i++) {
+//   // console.log(tableNumber, "x", i, "=", tableNumber * i);
+//   message = `${tableNumber} x ${i} = ${tableNumber * i}`;
+//   console.log(message);
+// }
+
+// window.onload = table();
+function table() {
+  let table = +prompt("Enter Number");
+  let tableResults;
+  for (let i = 1; i <= 10; i++) {
+    let tableResult = `${table} x ${i} = ${table * i}`;
+    console.log(tableResult);
+    tableResult += tableResult = "\n";
+    document.getElementById("table").innerText = tableResults;
   }
 }
 
-let maxNumber = 10;
-let genrateNumber = Math.floor(Math.random() * maxNumber + 1);
-let isCorrect = false;
-while (!isCorrect) {
-  let userNumber = +prompt(`Enter The Number! ${maxNumber}`);
-  if (userNumber === genrateNumber) {
-    isCorrect = true;
-    console.log("Congrulation! Your Guess is Correct!");
-  } else if (userNumber > genrateNumber) {
-    console.log("To High! Try Again");
-  } else {
-  }
-}
+// function table() {
+//   let table = +prompt("Enter Number");
+//   let tableResults = ""; // Accumulate results in this variable
+
+//   for (let i = 1; i <= 10; i++) {
+//     let tableResult = `${table} x ${i} = ${table * i}`;
+//     console.log(tableResult);
+//     tableResults += tableResult + "\n"; // Append each result to the variable
+//   }
+
+//   document.getElementById("table").innerText = tableResults; // Set inner text after the loop
+// }
