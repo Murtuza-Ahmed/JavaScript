514c87282aba499c8017277b586432bad3c793a8
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-03 11:08:00 +0500

    paractice

--- class-13/assits/javascript/app.js
+++ class-13/assits/javascript/app.js
@@ -144,3 +144,28 @@ let message = {};
 //     console.log("E VALUE", e);
 //   }
 // }
+
+// let blankArray = [];
+// for (let i = 0; 1 < 5; i++) {
+//   blankArray.push([]);
+//   for (let e = 0; e < 10; e++) {
+//     blankArray[i].push(e);
+//   }
+// }
+// console.log(blankArray);
+
+// let arrOfArrays = [];
+// for (let i = 0; i < 3; i++) {
+//   arrOfArrays.push([]);
+//   for (let j = 0; j < 7; j++) {
+//     arrOfArrays[i].push(j);
+//   }
+// }
+// console.log(arrOfArrays);
+
+// let number = 0;
+// while (number < 5) {
+//   console.log("hello", number);
+//   number++;
+// }
+
