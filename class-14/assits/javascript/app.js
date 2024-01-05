646c6d2e839cd2e91cc0a94f57293752d0c38269
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-05 20:54:01 +0500

    annonymeus function paractice

--- class-14/assits/javascript/app.js
+++ class-14/assits/javascript/app.js
@@ -6,8 +6,15 @@ function reveal(m) {
   console.log(m);
 }
 
-function hello() {
-  let user = prompt("What's Your Name");
-  console.log(`Hello ${user}!`);
-}
-hello();
+// function hello() {
+//   let user = prompt("What's Your Name");
+//   console.log(`Hello ${user}!`);
+// }
+// hello();
+
+let sayHello = function () {
+  let inFunction = "I'm in a Sheroz";
+  console.log(`Hello ${inFunction}`);
+};
+sayHello();
+sayHello();
