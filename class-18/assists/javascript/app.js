42db4ea8f40d63d7901534bcece916fecdf5e4bd
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-10 12:33:07 +0500

    paractice

--- class-18/assists/javascript/app.js
+++ class-18/assists/javascript/app.js
@@ -17,17 +17,33 @@ console.log(val);
 
 // let userNumber = +prompt("Enter The Number");
 // let userNumber1 = +prompt("Enter The Number");
-function addNumber(a, b) {
-    if (typeof a !== "number") {
-        throw alert("Please Enter Number");
-    } if (typeof b !== "number") {
-        throw alert("Please Correct Number");
-    }
-    return a + b;
-}
-try {
-    console.log(addNumber(2, 4));
+// function addNumber(a, b) {
+//     if (typeof a !== "number") {
+//         throw alert("Please Enter Number");
+//     } if (typeof b !== "number") {
+//         throw alert("Please Correct Number");
+//     }
+//     return a + b;
+// }
+// try {
+//     console.log(addNumber(2, 4));
 
-} catch (e) {
-    alert(e)
-}
+// } catch (e) {
+//     alert(e)
+// }
+
+// let valUserNumber = +prompt("Enter the NUmber");
+// function checkNumber(val) {
+//     try {
+//         if (isNaN(val)) {
+//             throw new Error("Not a number");
+//         } else {
+//             console.log("Got a number");
+//         }
+//     } catch (error) {
+//         console.error("Error:", error.message);
+//     } finally {
+//         console.log("Function completed. Value of val:", val);
+//     }
+// }
+// checkNumber(valUserNumber);
