34af5963f8f01a36d6c86aded92447b0f2d2e9ee
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-13 21:08:28 +0500

    react class

---
+++ custom-react/src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// const App = () => {
+//     return React.createElement(
+//         "div",
+//         {},
+//         React.createElement("h1", {}, "Adopt Me!")
+//     );
+// };
+const App = () => {
+  return (
+    <div>
+      <h1>ScoobyDoo</h1>
+    </div>
+  );
+};
+const Pet = () => {};
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
