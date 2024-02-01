3ccfb262d64d1fda461d4b9514fcbe8a876ed874
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-01 15:45:45 +0500

    new component searchparams

--- react/class-4-hooks/src/components/searchParam.jsx
+++ react/class-4-hooks/src/components/searchParam.jsx
@@ -1,19 +1,7 @@
 function SearchParam() {
   return (
-    <div className="search-params">
-      <form>
-        <label htmlFor="location">
-          <input
-            type="text"
-            name="search"
-            id="location"
-            placeholder="Enter Loaction"
-          />
-        </label>
-        <button>submit</button>
-      </form>
-    </div>
-  );
+    <div>SearchParam</div>
+  )
 }
 
-export default SearchParam;
+export default SearchParam
