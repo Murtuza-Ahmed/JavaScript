dcdf352528a087a288951061e4668ac445c62354
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-02 17:56:43 +0500

    search params paractice

--- react/class-4-hooks/src/components/SearchParams.jsx
+++ react/class-4-hooks/src/components/SearchParams.jsx
@@ -1,9 +1,18 @@
 function SearchParams() {
+  let location = "";
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
-          <input type="text" id="location" placeholder="Enter Location" />
+          <input
+            type="text"
+            id="location"
+            value={location}
+            onChange={(e) => {
+              location = e.target.value;
+            }}
+            placeholder="Enter Location"
+          />
         </label>
         <button>Submit</button>
       </form>
