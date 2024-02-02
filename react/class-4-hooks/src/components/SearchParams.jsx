76943d1924a9a2c4648b68079ac4eb799bd0b572
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-02 18:19:56 +0500

    search params one file add select

--- react/class-4-hooks/src/components/SearchParams.jsx
+++ react/class-4-hooks/src/components/SearchParams.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 function SearchParams() {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
   console.log(location);
+  console.log(animal);
   // let location = "";
   return (
     <div className="search-params">
@@ -14,6 +16,18 @@ function SearchParams() {
             onChange={(e) => setLocation(e.target.value)}
             placeholder="Enter Location"
           />
+        </label>
+        <label htmlFor="animal">
+          <select
+            name="animal"
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+            <option value="rabbit">Rabbit</option>
+          </select>
         </label>
         <button>Submit</button>
       </form>
