972e34e8962a00deffe3fa4037e85ae32774e147
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-26 12:32:30 +0500

    typescript paratcice

--- typescript/assets/js/app.js
+++ typescript/assets/js/app.js
@@ -1,10 +1,9 @@
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
-const button = document.querySelector("button");
-
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+var button = document.querySelector("button");
 function add(number1, number2) {
-    return number1 + number2
+    return number1 + number2;
 }
 button.addEventListener("click", function () {
-    console.log(add(input1.value, input2.value))
-})
+    console.log(add(+input1.value, +input2.value));
+});
---
+++ typescript/assets/js/app.ts
@@ -0,0 +1,10 @@
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button")!;
+
+// function add(number1: number, number2: number) {
+//     return number1 + number2
+// }
+// button.addEventListener("click", function () {
+//     console.log(add(+input1.value, +input2.value))
+// })
