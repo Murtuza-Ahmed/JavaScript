70a930ddf9e82883c3209cd4c82753f64618cae6
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-27 13:18:38 +0500

    classes paratice

--- typescript/class-2-TS/dist/app.js
+++ typescript/class-2-TS/dist/app.js
@@ -5,4 +5,25 @@ function log(data) {
 }
 // log("hello")
 button.addEventListener("click", log.bind(null, "hello"));
+/**
+ * CLASSES
+ */
+class Student {
+    constructor(n, r, e) {
+        this.skills = [];
+        this.name = n;
+        this.rollNo = r;
+        this.email = e;
+    }
+    addSkill(skill) {
+        this.skills.push(skill);
+    }
+    getSkill() {
+        return this.skills;
+    }
+}
+const studentDetail = new Student("sheroz", 26, "murtuza@gmail.com");
+studentDetail.addSkill("javascript");
+console.log(studentDetail.getSkill());
+console.log(studentDetail);
 //# sourceMappingURL=app.js.map
--- typescript/class-2-TS/src/app.ts
+++ typescript/class-2-TS/src/app.ts
@@ -6,3 +6,33 @@ function log(data: string) {
 
 // log("hello")
 button.addEventListener("click", log.bind(null, "hello"))
+
+
+
+/**
+ * CLASSES
+ */
+
+class Student {
+    name: string;
+    rollNo: number;
+    email: string;
+    private skills: string[] = [];
+
+    constructor(n: string, r: number, e: string) {
+        this.name = n;
+        this.rollNo = r;
+        this.email = e;
+    }
+    addSkill(skill: string) {
+        this.skills.push(skill)
+    }
+    getSkill() {
+        return this.skills;
+    }
+}
+
+const studentDetail = new Student("sheroz", 26, "murtuza@gmail.com");
+studentDetail.addSkill("javascript");
+console.log(studentDetail.getSkill());
+console.log(studentDetail)
