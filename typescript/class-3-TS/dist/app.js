6e388246958a52ad158f0d7233ab2dc8c8d662ad
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-27 17:48:11 +0500

    interface ts paractice

--- typescript/class-3-TS/dist/app.js
+++ typescript/class-3-TS/dist/app.js
@@ -1,2 +1,12 @@
 "use strict";
+// interface person {
+//     name: string;
+// };
+class Human {
+    constructor(name) {
+        this.name = name;
+    }
+    speak(sentence) {
+    }
+}
 //# sourceMappingURL=app.js.map
---
+++ typescript/class-3-TS/src/app.ts
@@ -0,0 +1,22 @@
+// interface person {
+//     name: string;
+// };
+
+// let sheroz: person;
+
+// sheroz = {
+//     name: "sheikh",
+// }
+
+interface person {
+    name: string;
+    speak(sentence: string): void;
+}
+class Human implements person {
+
+    constructor(public name: string) { }
+
+    speak(sentence: string): void {
+        
+    }
+}
