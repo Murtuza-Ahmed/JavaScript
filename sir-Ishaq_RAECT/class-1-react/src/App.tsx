b28a0358acf5f7186384024c7616110523ae3642
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-28 14:11:23 +0500

    class end end new component item call

--- sir-Ishaq_RAECT/class-1-react/src/App.tsx
+++ sir-Ishaq_RAECT/class-1-react/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import List from "./components/list/List";
 
 function App() {
-  const text = "Wellcome to my React App.";
+  // const text = "Wellcome to my React App.";
+  const text = ["hello", "wellcome", "good bay"];
   return (
     <div>
-      <List data={"Hello"} />
+      {/* <List data={"Hello"} /> */}
+      {/* <List data={text} /> */}
+      {/* <List data={"Continue..."} /> */}
       <List data={text} />
-      <List data={"Continue..."} />
     </div>
   );
 }
--- sir-Ishaq_RAECT/class-1-react/src/components/list/List.tsx
+++ sir-Ishaq_RAECT/class-1-react/src/components/list/List.tsx
@@ -1,6 +1,12 @@
-function List({data}: {data: string}) {
-  return <div>
-    {data}
-  </div>;
+import Item from "../item/Item";
+function List({ data }: { data: string[] }) {
+  const items = data.map((item) => <Item key={item} data={item} />);
+  console.log(items);
+  return (
+    <div>
+      {/* {data} */}
+      {items}
+    </div>
+  );
 }
 export default List;
