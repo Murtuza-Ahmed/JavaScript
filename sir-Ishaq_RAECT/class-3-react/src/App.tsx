ceb1e74b2bec2ee841dcd883bf31c286335553bf
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-28 18:36:59 +0500

    new component todo.tsx

--- sir-Ishaq_RAECT/class-3-react/src/App.tsx
+++ sir-Ishaq_RAECT/class-3-react/src/App.tsx
@@ -1,27 +1,25 @@
 import "./App.css";
-import List from "./components/list/List";
-import AddForm from "./components/addForm/AddForm";
 import { useState } from "react";
+import Todo from "./components/todo/Todo";
 
 function App() {
-  // const text = "Wellcome to my React App.";
-  // const text = ["hello", "wellcome", "good bay"];
   const [items, setItems] = useState<string[]>([
     "hello",
     "wellcome",
     "good bay",
   ]);
+  const [isAuth, setIsAuth] = useState<boolean>(false);
 
   const addItemHandler = (itemText: string) => {
     setItems([...items, itemText]);
   };
   return (
     <div>
-      {/* <List data={"Hello"} /> */}
-      {/* <List data={text} /> */}
-      {/* <List data={"Continue..."} /> */}
-      <AddForm add={addItemHandler} />
-      <List data={items} />
+      {isAuth ? (
+        <Todo addItemHandler={addItemHandler} items={items} />
+      ) : (
+        <div>Plaese Login</div>
+      )}
     </div>
   );
 }
---
+++ sir-Ishaq_RAECT/class-3-react/src/components/todo/Todo.tsx
@@ -0,0 +1,19 @@
+import AddForm from "../addForm/AddForm";
+import List from "../list/List";
+
+type TodoProps = {
+  addItemHandler: (a: string) => void;
+  items: string[];
+};
+
+function Todo({ addItemHandler, items }: TodoProps) {
+  return (
+    <>
+      <div>
+        <AddForm add={addItemHandler} />
+        <List data={items} />
+      </div>
+    </>
+  );
+}
+export default Todo;
