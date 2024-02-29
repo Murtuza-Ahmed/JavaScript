f61e13b3f24b4fc98dd6f3a233757ee317331fe4
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-29 16:21:04 +0500

    file change and install scss

--- sir-Ishaq_RAECT/class-4-react/src/App.tsx
+++ sir-Ishaq_RAECT/class-4-react/src/App.tsx
@@ -1,32 +1,7 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import AuthContext from "./contexts/AuthContext";
-import AuthHandler from "./components/authHandler/AuthHandler";
+import "./App.scss";
 
 function App() {
-  useEffect(() => {
-    console.log("useeffect");
-    const isLoggIn = localStorage.getItem("isLoggIn") === "1";
-    setIsAuth(isLoggIn);
-  }, []);
-  const [isAuth, setIsAuth] = useState<boolean>(false);
-
-  const contextValue = {
-    isLoggedIn: isAuth,
-    setAuthentication: (val: boolean) => {
-      localStorage.setItem("isLoggIn", val ? "1" : "0");
-      setIsAuth(val);
-    },
-  };
-
-  return (
-    <div>
-      <AuthContext.Provider value={contextValue}>
-        {/* {isAuth ? <Todo /> : <Login onLogin={setLoginHandler} />} */}
-        <AuthHandler />
-      </AuthContext.Provider>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
