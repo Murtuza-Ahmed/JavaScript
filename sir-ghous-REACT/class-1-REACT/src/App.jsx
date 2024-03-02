e7ad41003faab2729866dd06a3c900b94942dbd8
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-02 14:29:24 +0500

    new component form bootstarp

--- sir-ghous-REACT/class-1-REACT/src/App.jsx
+++ sir-ghous-REACT/class-1-REACT/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import BasicExample from "./assets/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [name, setName] = useState("Sheroz");
+  // const [value, setValue] = useState("");
+  // console.log(value);
   return (
     <>
-      
+      {/* <h1>{name}</h1>
+      <button onClick={() => setName("Sheikh")}>Update</button> */}
+      {/* <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <h1>{value}</h1>
+      <button onClick={() => setValue("")}>Reset</button> */}
+
+      {/* FORM CALL */}
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <BasicExample />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
