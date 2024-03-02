b96aabf62503c2cb2c616cd79392db3b7acba1de
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-02 14:45:10 +0500

    login and register

--- sir-ghous-REACT/class-1-REACT/src/App.jsx
+++ sir-ghous-REACT/class-1-REACT/src/App.jsx
@@ -5,29 +5,29 @@ import BasicExample from "./assets/Form";
 import Button from "react-bootstrap/esm/Button";
 
 function App() {
-  // const [name, setName] = useState("Sheroz");
-  // const [value, setValue] = useState("");
-  // console.log(value);
+  const [auth, setAuth] = useState("");
   return (
     <>
-      {/* <h1>{name}</h1>
-      <button onClick={() => setName("Sheikh")}>Update</button> */}
-      {/* <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <h1>{value}</h1>
-      <button onClick={() => setValue("")}>Reset</button> */}
-
       {/* FORM CALL */}
       <div className="container">
         <div className="row">
-          <div className="col">
-            <h1>LOGGIN FORM</h1>
-            <BasicExample />
-            <Button variant="link">Regester Now</Button>
-          </div>
+          {auth === "login" ? (
+            <div className="col">
+              <h1>LOGGIN FORM</h1>
+              <BasicExample />
+              <Button variant="link" onClick={() => setAuth("register")}>
+                Regester Now
+              </Button>
+            </div>
+          ) : (
+            <div className="col">
+              <h1>REGESTER NOW</h1>
+              <BasicExample />
+              <Button variant="link" onClick={() => setAuth("login")}>
+                Loggin Now
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </>
