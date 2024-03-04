df2085c113248c3d40ccc726e3f794fcbfe4ca7c
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-04 11:46:43 +0500

    template leterals

--- zubair-RAECT/class-1-ES-6/js/app.js
+++ zubair-RAECT/class-1-ES-6/js/app.js
@@ -14,6 +14,20 @@ function checkScopeLet(love) {
         const lastName = "Ahmed"
         console.log("My name is" + fullName + " " + lastName)
     }
-    console.log("My name is" + fullName + " " + lastName)
+    // console.log("My name is" + fullName + " " + lastName)
 }
 checkScopeLet(true);
+
+/**
+ * TEMPLATE LETERALS 
+ */
+
+// =============EC5================
+let fName = "abc";
+let lNAme = "def";
+console.log("My name is" + fName + " " + lNAme);
+
+// =============EC6=================
+let fulName = "abc";
+let lasNAme = "def";
+console.log(`My name is ${fulName} ${lasNAme}`);
