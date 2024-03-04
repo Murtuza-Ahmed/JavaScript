c5d55fe1d5d6cb759ee304a7735af0c22a542a2c
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-04 11:56:45 +0500

    rest pearmeter

--- zubair-RAECT/class-1-ES-6/js/app.js
+++ zubair-RAECT/class-1-ES-6/js/app.js
@@ -31,3 +31,23 @@ console.log("My name is" + fName + " " + lNAme);
 let fulName = "abc";
 let lasNAme = "def";
 console.log(`My name is ${fulName} ${lasNAme}`);
+
+/**
+ * RESTPARAMETER
+ */
+
+// ============EC5==========
+function sum(a, b) {
+    console.log(a, b);
+}
+sum(1, 2, 3, 4, 5);
+
+// ================EC6=================
+function fun(a, b, ...c) {
+    console.log(`${a} ${b}`);
+    console.log(c);
+    console.log(c[2]);
+    console.log(c.length);
+    console.log(c.indexOf("hockey"))
+}
+fun("circket", "football", "basketball", "hockey", "racegame");
