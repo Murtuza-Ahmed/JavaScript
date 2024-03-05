1c01c735a3d96dc14e5f93e5add70e364e1c36dd
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-05 13:29:40 +0500

    class-3 start and class-2 folder changing

---
+++ zubair-RAECT/class-3-REACT/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./components/SearchParams";
+// import Pet from "./components/Pet";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Adopt Me!</h1>
+      </div>
+      <SearchParams />
+
+      {/* <Pet name="Luna" animal="Dog" breed="Haveenes" /> */}
+    </>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
