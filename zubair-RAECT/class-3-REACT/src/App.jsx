369323ab2a0347e2a17be8c0b98c4b24762e9dcf
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-07 12:55:53 +0500

    one fetchpet component delete and class end

--- zubair-RAECT/class-3-REACT/src/App.jsx
+++ zubair-RAECT/class-3-REACT/src/App.jsx
@@ -3,22 +3,18 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./components/SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./components/Details";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </QueryClientProvider>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- zubair-RAECT/class-3-REACT/src/components/Details.jsx
+++ zubair-RAECT/class-3-REACT/src/components/Details.jsx
@@ -1,22 +1,10 @@
 import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import fetchPet from "./fetchPet";
-
 const Details = () => {
   const { id } = useParams();
-  const result = useQuery(["details", id], fetchPet);
-  console.log(result)
-  return
-
   return (
-    <div clasName="detail">
-      <div>
-        <h1>{Pet.name}</h1>
-        <h2>{`${pet.animal} - ${pet.breed} - ${pet.city} - ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
-        <p>{pet.description}</p>
-      </div>
-    </div>
+    <>
+      <h2>{id}</h2>
+    </>
   );
 };
 export default Details;
