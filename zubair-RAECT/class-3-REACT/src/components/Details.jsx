0ed2e8152ec847ef145848058feb6b09ca478211
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-07 12:52:52 +0500

    paractice

--- zubair-RAECT/class-3-REACT/src/components/Details.jsx
+++ zubair-RAECT/class-3-REACT/src/components/Details.jsx
@@ -1,10 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+
 const Details = () => {
   const { id } = useParams();
+  const result = useQuery(["details", id], fetchPet);
+  console.log(result)
+  return
+
   return (
-    <>
-      <h2>{id}</h2>
-    </>
+    <div clasName="detail">
+      <div>
+        <h1>{Pet.name}</h1>
+        <h2>{`${pet.animal} - ${pet.breed} - ${pet.city} - ${pet.state}`}</h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
   );
 };
 export default Details;
