759b7014a864bcc736b7001f742c5d878bda2e0c
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-01 13:27:36 +0500

    class-1 end

--- sir-ghous-class/class-1-JS/app.js
+++ sir-ghous-class/class-1-JS/app.js
@@ -17,7 +17,27 @@ console.log(result);
 
 var nameAndAddress = `My Name IS Sheroz Ahmed And
 My Soscity Is Beutiful Place`;
-window.alert(nameAndAddress);
+// window.alert(nameAndAddress);
+
+// const checkbox = document.getElementById('checkbox');
+// const alertBox = document.getElementById('alert');
+
+// checkbox.addEventListener('change', function () {
+//     if (this.checked) {
+//         showAlert();
+//     } else {
+//         hideAlert();
+//     }
+// });
+
+// function showAlert() {
+//     alertBox.style.display = 'block';
+//     setTimeout(hideAlert, 3000); // Hide alert after 3 seconds
+// }
+
+// function hideAlert() {
+//     alertBox.style.display = 'none';
+// }
 
 const checkbox = document.getElementById('checkbox');
 const alertBox = document.getElementById('alert');
@@ -31,10 +51,12 @@ checkbox.addEventListener('change', function () {
 });
 
 function showAlert() {
+    alertBox.innerHTML = '<span id="alertText">Checkbox is checked!</span><br><label for="alertCheckbox">Check this box:</label><input type="checkbox" id="alertCheckbox">';
     alertBox.style.display = 'block';
     setTimeout(hideAlert, 3000); // Hide alert after 3 seconds
 }
 
 function hideAlert() {
+    alertBox.innerHTML = '';
     alertBox.style.display = 'none';
 }
