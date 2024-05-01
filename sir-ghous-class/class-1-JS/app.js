8a8a1dba33fb5282a5a6bf6cabe14a6a1e2e7fd8
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-01 13:13:51 +0500

    paractice

--- sir-ghous-class/class-1-JS/app.js
+++ sir-ghous-class/class-1-JS/app.js
@@ -9,3 +9,32 @@ console.log(num);
 var num = null;
 num = 123;
 console.log(num);
+
+var num1 = 5;
+var num2 = 10;
+var result = num1 + num2;
+console.log(result);
+
+var nameAndAddress = `My Name IS Sheroz Ahmed And
+My Soscity Is Beutiful Place`;
+window.alert(nameAndAddress);
+
+const checkbox = document.getElementById('checkbox');
+const alertBox = document.getElementById('alert');
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        showAlert();
+    } else {
+        hideAlert();
+    }
+});
+
+function showAlert() {
+    alertBox.style.display = 'block';
+    setTimeout(hideAlert, 3000); // Hide alert after 3 seconds
+}
+
+function hideAlert() {
+    alertBox.style.display = 'none';
+}
