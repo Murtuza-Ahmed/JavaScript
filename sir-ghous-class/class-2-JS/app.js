46464c0cc02532ba2761c1abc1f89aea9b9d7d54
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-01 15:17:35 +0500

    paractice

---
+++ sir-ghous-class/class-2-JS/app.js
@@ -0,0 +1,15 @@
+var name = "SIT";
+var address = "Sector 11";
+
+document.write("<h5>" + name + address + "</h5>");
+document.write("<h1>Helo World</h1> <p>SMIT</p>");
+
+// var num = 1;
+// alert(num);
+// num = num + 2;
+// alert(num);
+// num = num + 4;
+// alert(num);
+
+// var alert = alert(true);
+// alert(alert);
