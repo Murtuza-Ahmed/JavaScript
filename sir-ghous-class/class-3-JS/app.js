0877cfee81d5374e42d362cb32f8ffee87c3d85e
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-03 11:06:00 +0500

    paractice

---
+++ sir-ghous-class/class-3-JS/app.js
@@ -0,0 +1,15 @@
+// var firstName = "Raza";
+// var lastName = "Ahmed";
+// console.log(firstName + " " + lastName);
+
+// var userName = prompt();
+// console.log(userName);
+
+// var number1 = +prompt();
+// var number2 = parseInt(prompt());
+// console.log(number1 + number2);
+// console.log(typeof (number2));
+
+
+// <!--        IF STATMENT          -->
+
