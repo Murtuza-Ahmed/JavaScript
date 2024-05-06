8a4837b978115df544462f4b58bbdd5faa85f8ff
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-06 16:00:11 +0500

    new component search params

--- CLASS-REACT/class-1-react/src/App.jsx
+++ CLASS-REACT/class-1-react/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breeds="Havanese" />
+      {/* <Pet name="Luna" animal="Dog" breeds="Havanese" /> */}
+      <SearchParams />
     </div>
   );
 };
