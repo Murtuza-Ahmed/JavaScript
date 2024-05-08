80189f4ddfb2c2b7bf69c7bfb3fec0b5547ac44e
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-08 11:20:15 +0500

    paractice

--- CLASS-REACT/class-1-react/src/SearchParams.jsx
+++ CLASS-REACT/class-1-react/src/SearchParams.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("");
+  const [animal, updateAnimal] = useState("");
+
   console.log(location);
   return (
     <div className="search-params">
@@ -15,6 +18,28 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => updateLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              updateAnimal(e.target.value);
+              // setBreed("");
+            }}
+            onBlur={(e) => {
+              updateAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            {/* <option /> */}
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
