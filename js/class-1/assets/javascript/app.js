4bf800834881e4067f9d9f03553c0a6c82304ca1
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-20 11:14:27 +0500

    leet code problem paractice

--- js/class-1/assets/javascript/app.js
+++ js/class-1/assets/javascript/app.js
@@ -1,5 +1,36 @@
-alert("Hello Murtuza");
-let consoleName = "Hello Ahmed";
-console.log(consoleName);
-var number = 0;
-console.log(number);
+// alert("Hello Murtuza");
+// let consoleName = "Hello Ahmed";
+// console.log(consoleName);
+// var number = 0;
+// console.log(number);
+
+// const arr = [1, 2, 3];
+// arr.last();
+
+console.log(Array.prototype)
+
+Array.prototype.last = function () {
+    if (this.length === 0) {
+        return -1
+    } else {
+        return this[this.length - 1]
+    }
+}
+const arr = [1, 2, 3];
+arr.last()
+console.log(arr.last())
+
+
+
+
+var createCounter = function (n) {
+
+    return function () {
+        n + 1
+    }
+}
+
+const counter = createCounter(10)
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
