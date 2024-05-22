1fe855243b50cbdfc3cf652b7cd20f0085ca4c32
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-22 18:53:46 +0500

    paractice clouser function

--- js/leetCodeParactice/app.js
+++ js/leetCodeParactice/app.js
@@ -27,9 +27,51 @@ newFunction("ander");
 function counter() {
     let count = 0
     return function () {
-        count += 1
+        count++
         return count
     }
 }
 const counterFunction = counter();
 console.log(counterFunction());
+console.log(counterFunction());
+
+// 
+
+function add(a) {
+    return function (b) {
+        return a + b
+    }
+}
+
+const addition = add(5);
+console.log(addition(10));
+
+// 
+
+function nameFunc(fullName) {
+    console.log("fullname", fullName)
+    return function (lastName) {
+        console.log("lastname", lastName)
+        return fullName + " " + lastName
+        // return function (otherName) {
+        //     console.log("other", otherName)
+        //     return fullName + " " + lastName + " " + otherName
+        // }
+    }
+}
+
+const userName = nameFunc("Raza");
+console.log(userName("Ahmed"));
+// console.log(userName("sheroz"));
+
+// 
+
+// function fetchData(url) {
+//     let data = null;
+//     fetch(url).then(response => {
+//         data = response.json();
+//         console.log(data); // Closure variable ko access karna
+//     });
+// }
+
+// fetchData('https://api.example.com/data');
