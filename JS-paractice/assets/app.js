10d632bd525e60ef97140281743c948f1cda71c0
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-07-24 19:14:55 +0500

    complete palindrom game

--- JS-paractice/assets/app.js
+++ JS-paractice/assets/app.js
@@ -1,3 +1,7 @@
+/**
+ * REVERCE STRING PARACTICE
+ */
+
 let string = "hello world take help care";
 let string1 = "javascript";
 
@@ -6,7 +10,7 @@ function strReverce() {
     let strRevec = string.split(" ");
     let arrRevec = strRevec.reverse();
     let arrJoin = arrRevec.join();
-    console.log(arrJoin)
+    // console.log(arrJoin)
 }
 
 strReverce();
@@ -15,7 +19,21 @@ function strReverce1(str) {
     let strRev = str.split("");
     let arrRev = strRev.reverse();
     let joinRev = arrRev.join(" ");
-    console.log(joinRev)
+    // console.log(joinRev)
 }
 
 strReverce1(string1)
+
+/**
+ * PALINDROM PARACTICE
+ */
+const buttonCheckPalindrom = document.getElementById("check");
+
+buttonCheckPalindrom.addEventListener("click", () => {
+    const userInput = prompt('Enter the Palindrom-Game');
+    const acceptInput = userInput;
+    const str = acceptInput.split("");
+    const strReverse = str.reverse();
+    const strJoin = strReverse.join("");
+    acceptInput === strJoin ? alert('welldone') : alert("no't Palindrom");
+});
