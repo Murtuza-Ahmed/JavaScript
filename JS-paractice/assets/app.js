379f8baa4b9adfde1e7d6e02715bfa80873942ef
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-07-25 00:20:01 +0500

    array paractice

--- JS-paractice/assets/app.js
+++ JS-paractice/assets/app.js
@@ -37,3 +37,52 @@ buttonCheckPalindrom.addEventListener("click", () => {
     const strJoin = strReverse.join("");
     acceptInput === strJoin ? alert('welldone') : alert("no't Palindrom");
 });
+
+/**
+ * FIND LARGEST NUMBER
+ */
+
+const arrNumber = [1, 2, 3, 4, 5];
+
+const buttonCheckNumber = document.getElementById("check-number-1");
+
+buttonCheckNumber.addEventListener("click", function () {
+    const largeNumber = Math.max(...arrNumber);
+    console.log('large-number', largeNumber);
+});
+
+const arrNum = [22, 45, 89, 40, 12, 85, 35, 3, 78];
+
+const buttonCheckNumber2 = document.getElementById("check-number-2");
+
+function findLargeNumber(arrNum) {
+    return Math.max(...arrNum)
+}
+
+buttonCheckNumber2.addEventListener("click", function () {
+    const callFunc = findLargeNumber(arrNum);
+    console.log(callFunc)
+});
+
+
+const arrNum1 = [22, 45, 89, 40, 12, 85, 35, 3, 78, 23, 45, 88, 49, 11, 19, 36, 35, 73];
+
+const buttonCheckNumber3 = document.getElementById("check-number-3");
+
+function arrNumFunc(arr) {
+    if (arr.length === 0) {
+        return null
+    }
+
+    let largest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i]
+        }
+    }
+    console.log(largest)
+}
+
+buttonCheckNumber3.addEventListener("click", function () {
+    arrNumFunc(arrNum1)
+})
